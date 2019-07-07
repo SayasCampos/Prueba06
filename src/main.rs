ec54421eca0e62c9d0ce79b53c0b1052ce2b24b8
tango <cteters@pdx.edu>
2019-07-07 11:21:16 -0700

    A basic start point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate rodio;
+
+use std::io::BufReader;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let device = rodio::default_output_device().unwrap();
+
+    let file = std::fs::File::open("examples/victory.mp3").unwrap();
+    let victory = rodio::play_once(&device, BufReader::new(file)).unwrap();
+    victory.set_volume(1.0);
+    println!("victory");
+
+    thread::sleep(Duration::from_millis(4500));
 }
