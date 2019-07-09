4bb70ae1b95596a637347298923fa98de3712789
maxjaspersmiley <max.jasper.smiley@gmail.com>
2019-07-09 16:15:20 -0700

    unfinished - working on audio

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,67 @@
-extern crate rodio;     // digital audio coding
-extern crate iron;      // high level web framwork
-extern crate router;    // routing for the iron crate
-extern crate urlencoded;use std::str::FromStr; // middleware for iron
-#[macro_use] extern crate mime; // basic web graphics
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate mime;
+extern crate rodio;
 
 use std::io::BufReader;
 use std::thread;
 use std::time::Duration;
-use iron::prelude::*;
-use iron::status;
-use router::Router;
-use urlencoded::UrlEncodedBody;
 
-fn main() {
-    let mut router = Router::new();
+use std::fs::File;
+use std::io::prelude::*;
 
-    router.get("/", get_from, "index");
-    router.post("/player", player_funct, "player");
 
-    println!("Serving on http://localhost:3000...");
-    Iron::new(router).http("localhost:3000").unwrap();
+use std::io;
+use rocket::request::{Form, FormError, FormDataError};
+use rocket::response::NamedFile;
+use rocket::http::RawStr;
 
-}
 
-fn get_from(_request: &mut Request) -> IronResult<Response> {
-    let mut response = Response::new();
-
-    response.set_mut(status::Ok);
-    response.set_mut(mime!(Text/Html; Charset=Utf8));
-    response.set_mut(r#"
-        <title>Rustadio Media Player</title>
-        <form action="/player" method="post">
-            <button type="submit">Prev</button>
-            <button type="submit">Play/Pause</button>
-            <button type="submit">Stop</button>
-            <button type="submit">Next</button>
-            <button type="submit">Vol +</button>
-            <button type="submit">Vol -</button>
-        </form>
-    "#);
-
-    Ok(response)
-}
-
-fn player_funct(request: &mut Request) -> IronResult<Response> {
-
+/*
 
     let device = rodio::default_output_device().unwrap();
+    let file = std::fs::File::open("victory.mp3").unwrap();
+    let victory = rodio::play_once(&device, BufReader::new(file)).unwrap();
+    victory.set_volume(1.0);
+    println!("victory!");
 
+#[derive(Debug, FromForm)]
+struct FormInput {
+    a: String,
+    b: bool,
+}
+
+
+#[post("/", data = "<play>")]
+fn play(play: Result<Form<FormInput>, FormError>) -> String {
+    match play {
+        Ok(form) => format!("{:?}", &*form),
+        Err(FormDataError::Io(_)) => format!("Form input was invalid UTF-8"),
+        Err(FormDataError::Malformed(f)) | Err(FormDataError::Parse(_, f)) => {
+            format!("Invalid form input: {}", f)
+        }
+    }
+}
+*/
+
+#[post("/play")]
+fn play() {
+    let device = rodio::default_output_device().unwrap();
     let file = std::fs::File::open("examples/victory.mp3").unwrap();
     let victory = rodio::play_once(&device, BufReader::new(file)).unwrap();
     victory.set_volume(1.0);
-    println!("victory");
+    println!("Victory!");
+}
 
-    thread::sleep(Duration::from_millis(4500));
+#[get("/")]
+fn index() -> io::Result<NamedFile> {
+    NamedFile::open("static/index.html")
+}
 
-    
-    let mut response = Response::new();
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index, play])
+}
 
-
-    response.set_mut(status::Ok);
-    response.set_mut(mime!(Text/Html; Charset=Utf8));
-    response.set_mut(
-        format!("Playing track\n"));
-    Ok(response)
+fn main() {
+    rocket().launch();
 }
