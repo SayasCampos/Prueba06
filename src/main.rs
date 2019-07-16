cffc3cf32363d14a961d7cbf9e26d24bffe27e61
tango <cteters@pdx.edu>
2019-07-16 15:06:24 -0700

    Bootstrap now works with Rocket!

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 #![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use] extern crate rocket;
-#[macro_use] extern crate mime;
+#[macro_use] extern crate serde_derive;
+extern crate rocket_contrib;
+
+use rocket::Rocket;
+use rocket::request::{FlashMessage};
+use rocket_contrib::{templates::Template, serve::StaticFiles};
+
 
 use std::io::BufReader;
 use std::thread;
 use std::time::Duration;
 
-use std::fs::File;
-use std::io::prelude::*;
 
-
-use std::io;
-use rocket::request::{Form, FormError, FormDataError};
-use rocket::response::NamedFile;
-use rocket::http::RawStr;
-
-
-
-#[get("/v", rank=2)]
+#[get("/v")]
 fn play_victory() {
     let device = rodio::default_output_device().unwrap();
     let file = std::fs::File::open("media/victory.mp3").unwrap();
@@ -28,42 +25,48 @@ fn play_victory() {
     let tag = id3::Tag::read_from_path("media/victory.mp3").unwrap();
     println!("{} {}", tag.title().unwrap() , tag.album().unwrap());
 
-    
 
-
-    NamedFile::open("static/index.html");
     thread::sleep(Duration::from_millis(4500));
 }
 
-#[get("/j", rank=1)]
-fn play_jiggly() {
-    let device = rodio::default_output_device().unwrap();
-    let file = std::fs::File::open("media/jiggly.mp3").unwrap();
-    let victory = rodio::play_once(&device, BufReader::new(file)).unwrap();
-    victory.set_volume(1.0);
-    println!("JIGGLY\n");
-    thread::sleep(Duration::from_millis(8000));
+
+#[derive(Debug, Serialize)]
+struct Context<'a, 'b>{ msg: Option<(&'a str, &'b str)> }
+
+impl<'a, 'b> Context<'a, 'b> {
+    pub fn err(msg: &'a str) -> Context<'static, 'a> {
+        Context{msg: Some(("error", msg))}
+    }
+
+    pub fn raw(msg: Option<(&'a str, &'b str)>) -> Context<'a, 'b> {
+        Context{msg: msg}
+    }
 }
 
-#[get("/", rank=3)]
+
+/*
+#[get("/")]
 fn index() -> io::Result<NamedFile> {
     NamedFile::open("static/index.html")
 }
-
-/*
-#[get("media/<file..>")]
-fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("static/").join(file)).ok()
-}
 */
 
+#[get("/")]
+fn index(msg: Option<FlashMessage<'_, '_>>) -> Template {
+    Template::render("index", &match msg {
+        Some(ref msg) => Context::raw(Some((msg.name(), msg.msg()))),
+        None => Context::raw(None),
+    })
+}
 
-fn rocket() -> rocket::Rocket {
-    rocket::ignite().mount("/", routes![index, play_victory, play_jiggly])
+fn rocket() -> Rocket {
+    rocket::ignite()
+        .mount("/", StaticFiles::from("static/"))
+	.mount("/", routes![index, play_victory])
+        .attach(Template::fairing())
 }
 
 fn main() {
-
     // Example playlist entry
     let playlist = vec![
         m3u::path_entry(r"Alternative\Band - Song.mp3"),
@@ -76,11 +79,11 @@ fn main() {
     ];
     println!("There are {} items in current playlist", playlist.len());
 
-
     // Example playlist I/O
     let mut reader = m3u::Reader::open("media/playlist.m3u").unwrap();
     let read_playlist: Vec<_> = reader.entries().map(|entry| entry.unwrap()).collect();
     println!("Uploaded {} tracks to a playlist", read_playlist.len());
-    
+
+
     rocket().launch();
 }
